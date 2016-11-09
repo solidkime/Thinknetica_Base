436f37a6ba1cf718ec596e6fe66ba31876e628c0
solidkime <dr.kime@gmail.com>
2016-11-09 16:41:43 +0200

    fix tabs

--- Lesson_1/quadratic_equation.rb
+++ Lesson_1/quadratic_equation.rb
@@ -10,18 +10,17 @@ c = gets.to_f
 
 def quad_eq(a,b,c)
   d = (b**2) - 4*a*c
-    if d < 0
-  		puts "Корней нет. D = #{d}"
-  	elsif d > 0
-      d_sqrt = Math.sqrt(d)
-  		x1 = (-b + d_sqrt ) / 2*a
-  		x2 = (-b - d_sqrt / 2*a
-
-  		puts "D = #{d}, x1 = #{x1}, x2 = #{x2}"
-  	elsif d == 0
-  		x = -b/(2*a)
-  		puts "D = #{d}, x1 и x2 = #{x}"
-  	end
+  if d < 0
+    puts "Корней нет. D = #{d}"
+  elsif d > 0
+    d_sqrt = Math.sqrt(d)
+    x1 = (-b + d_sqrt ) / 2*a
+    x2 = (-b - d_sqrt / 2*a
+      puts "D = #{d}, x1 = #{x1}, x2 = #{x2}"
+  elsif d == 0
+    x = -b/(2*a)
+    puts "D = #{d}, x1 и x2 = #{x}"
+  end
 end
 
 quad_eq(a,b,c)
