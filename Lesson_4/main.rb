00854b99c9392836ec4ae9d407a41b393f56aa20
solidkime <dr.kime@gmail.com>
2016-11-23 02:30:45 +0200

    made Railway class

--- Lesson_4/main.rb
+++ Lesson_4/main.rb
@@ -7,158 +7,159 @@ require_relative 'wagon'
 require_relative 'passenger_wagon'
 require_relative 'cargo_wagon'
 
-def menu
+class Railway
+  def menu
 
-  puts "1. Создание станции"
-  puts "2. Создание поезда"
-  puts "3. Добавление вагонов к поезду, отсоединение вагонов от поезда и помещение поезда на станцию"
-  puts "4. Вывод списка станций и список поездов на станции"
-  print ' > '
+    puts "1. Создание станции"
+    puts "2. Создание поезда"
+    puts "3. Добавление вагонов к поезду, отсоединение вагонов от поезда и помещение поезда на станцию"
+    puts "4. Вывод списка станций и список поездов на станции"
+    print ' > '
 
-  request = gets.chomp.to_i
+    request = gets.chomp.to_i
 
-  case request
-  when 1
-    create_station
-  when 2
-    create_train
-  when 3
-    edit_train
-  when 4
-    list
-  else
-    puts "Вы ввели что-то не то!"
-    exit
-  end
-end
-
-def create_station
-  @stations ||= {}
-  @index_stations ||= 0
-
-  loop do
-    puts "Для выхода ничего не вводите и нажмите Enter"
-    puts "Введите название станции:"
-    print '>'
-    station_name = gets.chomp
-    
-    if station_name == ""
-      puts @stations
-      menu
-    else
-      @stations[@index_stations] = Station.new(station_name)
-      @index_stations += 1
-    end
-  end
-end
-
-def create_train
-  @trains ||= {}
-  @index_trains  ||= 0
-
-  loop do
-    puts "Для выхода ничего не вводите и нажмите Enter"
-    puts "Выберите тип поезда:"
-    puts "1. Пассажирский"
-    puts "2. Грузовой"
-    train_type = gets.chomp.to_i
-    print '>'
-
-    case train_type
+    case request
     when 1
-      puts "Создаем пассажирский поезд"
-      puts "Введите номер поезда"
-      train_id = gets.chomp.to_i
-      print '>'
-      @trains[@index_trains] = Train.new(train_id, :passenger)
-      @index_trains += 1
+      create_station
     when 2
-      puts "Создаем грузовой поезд"
-      puts "Введите номер поезда"
-      train_id = gets.chomp.to_i
-      print '>'
-      @trains[@index_trains] = Train.new(train_id, :cargo)
-      @index_trains += 1
-    when 0
-      puts @trains
-      menu
-    end
-  end
-end
-
-def edit_train
-
-  puts "Выберите поезд с которым будем проводить операции:"
-  puts "Чтобы выбрать поезд введите его порядковый номер или оставьте поле пустым, чтобы вернуться в предыдущее меню:"
- 
-  @trains.each do |index, train|
-    puts "#{index}. айди поезда:#{train.id}, тип поезда: #{train.type}"
-  end
-
-   print ">"
-
-  request = gets.chomp.to_i
-
-  if @trains[request]
-    train = @trains[request]
-  else
-    "Такого поезда нет в списке."
-    menu
-  end
-
-  puts "Вы выбрали поезд #{train}. Какую операцию хотите провести?"
-  puts "1. Прицепить вагон"
-  puts "2. Отцепить вагон"
-  puts "3. Отправить поезд на станцию"
-
-  request = gets.chomp.to_i
-
-  case request
-  when 1
-    if train.type == :passenger
-      wagon = PassengerWagon.new
-    elsif train.type == :cargo
-      wagon = CargoWagon.new
-    end
-    train.wagon_plus(wagon)
-  when 2
-    train.wagon_minus
-  when 3
-    puts "Можно поместить поезд на такие станции:"
-    @stations.each do |index, station|
-      puts "#{id} #{station.name}"
-    end
-    puts "Введите номер станции или нажмите Enter для выхода"
-
-    station_number = gets.chomp.to_i
-
-    if @stations.has_key?(station_number)
-      station = @stations[station_number]
-      #или здесь стоит сделать добавление станции к атрибутам поезда? Без роута это конечно не очень.
-      station.plus_train(train_id)
+      create_train
+    when 3
+      edit_train
+    when 4
+      list
     else
-      puts "Такой станции не существует"
-      start
+      puts "Вы ввели что-то не то!"
+      exit
     end
-    
-  when 0
-    puts "Вы ввели что-то не то"
-    menu
   end
+
+
+  private
+
+  def create_station
+    @stations ||= {}
+    @index_stations ||= 0
+
+    loop do
+      puts "Для выхода ничего не вводите и нажмите Enter"
+      puts "Введите название станции:"
+      print '>'
+      station_name = gets.chomp
+      
+      if station_name == ""
+        puts @stations
+        menu
+      else
+        @stations[@index_stations] = Station.new(station_name)
+        @index_stations += 1
+      end
+    end
+  end
+
+  def create_train
+    @trains ||= {}
+    @index_trains  ||= 0
+
+    loop do
+      puts "Для выхода ничего не вводите и нажмите Enter"
+      puts "Выберите тип поезда:"
+      puts "1. Пассажирский"
+      puts "2. Грузовой"
+      train_type = gets.chomp.to_i
+      print '>'
+
+      case train_type
+      when 1
+        puts "Создаем пассажирский поезд"
+        puts "Введите номер поезда"
+        train_id = gets.chomp.to_i
+        print '>'
+        @trains[@index_trains] = Train.new(train_id, :passenger)
+        @index_trains += 1
+      when 2
+        puts "Создаем грузовой поезд"
+        puts "Введите номер поезда"
+        train_id = gets.chomp.to_i
+        print '>'
+        @trains[@index_trains] = Train.new(train_id, :cargo)
+        @index_trains += 1
+      when 0
+        puts @trains
+        menu
+      end
+    end
+  end
+
+  def edit_train
+
+    puts "Выберите поезд с которым будем проводить операции:"
+    puts "Чтобы выбрать поезд введите его порядковый номер или оставьте поле пустым, чтобы вернуться в предыдущее меню:"
+   
+    @trains.each do |index, train|
+      puts "#{index}. айди поезда:#{train.id}, тип поезда: #{train.type}"
+    end
+
+     print ">"
+
+    request = gets.chomp.to_i
+
+    loop do
+
+      if @trains[request]
+        train = @trains[request]
+      else
+        "Такого поезда нет в списке."
+        menu
+      end
+
+      puts "Вы выбрали поезд #{train.id}. Какую операцию хотите провести?"
+      puts "1. Прицепить вагон"
+      puts "2. Отцепить вагон"
+      puts "3. Отправить поезд на станцию"
+
+      request = gets.chomp.to_i
+
+      case request
+      when 1
+        if train.type == :passenger
+          wagon = PassengerWagon.new
+        elsif train.type == :cargo
+          wagon = CargoWagon.new
+        end
+        train.wagon_plus(wagon)
+      when 2
+        train.wagon_minus
+      when 3
+        puts "Можно поместить поезд на такие станции:"
+        @stations.each do |index, station|
+          puts "#{id} #{station.name}"
+        end
+        puts "Введите номер станции или нажмите Enter для выхода"
+
+        station_number = gets.chomp.to_i
+
+        if @stations.has_key?(station_number)
+          station = @stations[station_number]
+          #или здесь стоит сделать добавление станции к атрибутам поезда? Без роута это конечно не очень.
+          station.plus_train(train) #!!!
+        else
+          puts "Такой станции не существует"
+          menu
+        end
+        
+      when 0
+        puts "Вы ввели что-то не то"
+        menu
+      end
+    end
+  end
+
+  def list
+    puts @stations
+    puts @trains
+  end
+
 end
-
-def list
-  puts @stations
-  puts @trains
-end
-
-
-
-
-menu
-  
-# Можно добавить еще много приятной косметики вроде правильного вывода номеров поезда или красивого вывода списов,
-# но думаю это не критично.
 
 
 
